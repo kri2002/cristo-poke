aac9c340f202a13cc3c302a70e47f423a761f7ab
Cristo <2121100622@soy.utj.edu.mx>
2025-10-03 00:29:11 -0600

    chore: actualizar dependencias y configuraciones del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pokemon-red': '#DF2D24',
+        'pokemon-blue': '#2A75BB',
+        'pokemon-yellow': '#FFCB05',
+      },
+    },
   },
   plugins: [],
 };
