1060f97d6ca35ec9e8ba8a40442494270f5501b0
Cristo <2121100622@soy.utj.edu.mx>
2025-10-03 04:28:31 -0600

    feat(pokemon-of-the-day): agregar seccion del pokemon del dia

--- src/components/pokemon/FeaturedPokemon.jsx
+++ src/components/pokemon/FeaturedPokemon.jsx
@@ -1,36 +1,142 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
+import { getPokemonCryUrl } from "../../services/pokeapi";
+import { GiSpeaker } from "react-icons/gi";
 
 const FeaturedPokemon = ({ pokemon }) => {
   if (!pokemon) return null;
 
-  const { name, sprites, types } = pokemon;
+  const { name, sprites, types, stats, id } = pokemon;
+
+  const handlePlayCry = () => {
+    const audio = new Audio(getPokemonCryUrl(id));
+    audio.play();
+  };
+
+  const statVariants = {
+    hidden: { width: 0 },
+    visible: (custom) => ({
+      width: `${(custom.base_stat / 255) * 100}%`,
+      transition: {
+        duration: 1,
+        delay: 1.2 + custom.index * 0.15,
+      },
+    }),
+  };
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.7, delay: 0.6 }}
-      className="bg-white/30 backdrop-blur-md rounded-xl p-6 shadow-lg text-center"
-    >
-      <h3 className="text-2xl font-bold text-gray-800 mb-2">Pokémon del Día</h3>
-      <img
-        src={sprites.other['official-artwork'].front_default || sprites.front_default}
-        alt={name}
-        className="w-48 h-48 mx-auto"
-      />
-      <h4 className="text-3xl font-bold capitalize mt-2 text-gray-900">{name}</h4>
-      <div className="flex justify-center mt-2">
-        {types.map((typeInfo) => (
-          <span
-            key={typeInfo.type.name}
-            className="text-sm font-semibold mr-2 px-3 py-1 rounded-full bg-gray-800 text-white"
-          >
-            {typeInfo.type.name}
-          </span>
-        ))}
-      </div>
-    </motion.div>
+    <>
+      <h3
+        className="text-5xl text-gray-800 mb-28 tracking-widest font-pokemon text-pokemon-yellow"
+        style={{
+          textShadow: `
+              -2px -2px 0 #2A75BB, 2px -2px 0 #2A75BB,
+              -2px 2px 0 #2A75BB, 2px 2px 0 #2A75BB,
+              3px 3px 6px rgba(0,0,0,0.7)
+            `,
+        }}
+      >
+        Pokémon del Día
+      </h3>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7, delay: 0.6 }}
+        className="min-h-full min-w-full flex justify-center items-center p-6"
+      >
+        <div className="flex flex-col md:flex-row items-center md:items-stretch gap-10 w-full max-w-screen">
+          {/* Sección izquierda: Imagen, nombre, grito y tipos */}
+
+          <div className="bg-white/30 backdrop-blur-md rounded-xl shadow-lg flex flex-col items-center justify-center p-4 w-full md:w-2/4 md:border-r border-gray-400/50">
+            <img
+              src={
+                sprites.other["official-artwork"].front_default ||
+                sprites.front_default
+              }
+              alt={name}
+              className="w-48 h-48 mx-auto"
+            />
+            <h4
+              className="text-3xl font-bold capitalize mt-2 mb-4 text-gray-900 tracking-widest font-pokemon text-pokemon-yellow"
+              style={{
+                textShadow: `
+              -2px -2px 0 #2A75BB, 2px -2px 0 #2A75BB,
+              -2px 2px 0 #2A75BB, 2px 2px 0 #2A75BB,
+              3px 3px 6px rgba(0,0,0,0.7)
+            `,
+              }}
+            >
+              {name}
+            </h4>
+            <button
+              onClick={handlePlayCry}
+              className="absolute bottom-4 right-4 text-white hover:text-pokemon-yellow transition-colors"
+            >
+              <GiSpeaker className="h-14 w-14" />
+            </button>
+            <div className="flex justify-center mt-2">
+              {types.map((typeInfo) => (
+                <span
+                  key={typeInfo.type.name}
+                  className="text-sm font-semibold mr-2 px-3 py-1 rounded-full bg-gray-800 text-white"
+                >
+                  {typeInfo.type.name}
+                </span>
+              ))}
+            </div>
+          </div>
+
+          {/* Sección derecha: Estadísticas Base */}
+          <div className="bg-white/30 backdrop-blur-md rounded-xl shadow-lg flex flex-col items-center w-full md:w-2/4 p-4 md:pl-8">
+            <h3
+              className="text-2xl font-bold mb-2 tracking-widest font-pokemon text-pokemon-yellow"
+              style={{
+                textShadow: `
+              -2px -2px 0 #2A75BB, 2px -2px 0 #2A75BB,
+              -2px 2px 0 #2A75BB, 2px 2px 0 #2A75BB,
+              3px 3px 6px rgba(0,0,0,0.7)
+            `,
+              }}
+            >
+              Estadísticas Base
+            </h3>
+            <div className="mt-4 w-full h-full flex flex-col justify-center">
+              {stats.map((stat, index) => (
+                <div
+                  key={stat.stat.name}
+                  className="flex items-center justify-between mb-2"
+                >
+                  <span className="w-1/3 text-sm capitalize text-left text-pokemon-yellow font-bold">
+                    {stat.stat.name}
+                  </span>
+                  <div className="flex-1 bg-gray-200 rounded-full h-2.5 mx-2">
+                    <motion.div
+                      className="bg-blue-600 h-2.5 rounded-full"
+                      variants={statVariants}
+                      initial="hidden"
+                      animate="visible"
+                      custom={{ base_stat: stat.base_stat, index: index }}
+                    ></motion.div>
+                  </div>
+                  <span
+                    className="ml-2 text-sm text-righttracking-widest font-pokemon text-pokemon-yellow"
+                    style={{
+                      textShadow: `
+              -2px -2px 0 #2A75BB, 2px -2px 0 #2A75BB,
+              -2px 2px 0 #2A75BB, 2px 2px 0 #2A75BB,
+              3px 3px 6px rgba(0,0,0,0.7)
+            `,
+                    }}
+                  >
+                    {stat.base_stat}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
-// src/pages/HomePage.jsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { fetchRandomPokemon } from "../services/pokeapi";
+import FeaturedPokemon from "../components/pokemon/FeaturedPokemon";
+import Loader from "../components/common/Loader";
 
 // 1. Importamos las imágenes de los Pokémon
 import pikachu from "../assets/images/pokemon/pikachu.png";
@@ -12,102 +14,139 @@ import geodude from "../assets/images/pokemon/geodude.png";
 import mewto from "../assets/images/pokemon/mewto.png";
 
 const HomePage = () => {
+  const [randomPokemon, setRandomPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadRandomPokemon = async () => {
+      try {
+        setLoading(true);
+        const pokemonData = await fetchRandomPokemon();
+        setRandomPokemon(pokemonData);
+      } catch (error) {
+        console.error("Error al cargar el Pokémon del día:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadRandomPokemon();
+  }, []);
   return (
-    // 2. Añadimos 'relative' para posicionar las imágenes y 'overflow-x-hidden' para ocultar la animación inicial
-    <div className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-home-bg p-4 overflow-x-hidden">
-      
-      {/* --- Imágenes de la Izquierda --- */}
-      <motion.img
-        src={pikachu}
-        alt="Pikachu"
-        className="absolute bottom-4 -left-20 md:-left-24 w-64 h-64 md:w-60 md:h-60 z-0"
-        initial={{ x: "-100%", opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ type: "spring", stiffness: 40, delay: 0.8 }}
-      />
-      <motion.img
-        src={bulbasaur}
-        alt="Bulbasaur"
-        className="absolute top-1/3 -left-16 md:-left-20 w-48 h-48 md:w-52 md:h-52 z-0"
-        initial={{ x: "-100%", opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ type: "spring", stiffness: 40, delay: 0.6 }}
-      />
-      <motion.img
-        src={geodude}
-        alt="Geodude"
-        className="absolute top-8 -left-12 md:-left-16 w-40 h-40 md:w-48 md:h-48 z-0"
-        initial={{ x: "-100%", opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ type: "spring", stiffness: 40, delay: 0.4 }}
-      />
+    <>
+      <div className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-home-bg p-4 overflow-x-hidden">
+        {/* --- Imágenes de la Izquierda --- */}
+        <motion.img
+          src={pikachu}
+          alt="Pikachu"
+          className="absolute bottom-4 -left-20 md:-left-24 w-64 h-64 md:w-60 md:h-60 z-0"
+          initial={{ x: "-100%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 40, delay: 0.8 }}
+        />
+        <motion.img
+          src={bulbasaur}
+          alt="Bulbasaur"
+          className="absolute top-1/3 -left-16 md:-left-20 w-48 h-48 md:w-52 md:h-52 z-0"
+          initial={{ x: "-100%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 40, delay: 0.6 }}
+        />
+        <motion.img
+          src={geodude}
+          alt="Geodude"
+          className="absolute top-8 -left-12 md:-left-16 w-40 h-40 md:w-48 md:h-48 z-0"
+          initial={{ x: "-100%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 40, delay: 0.4 }}
+        />
 
-      {/* --- Imágenes de la Derecha --- */}
-      <motion.img
-        src={charizard}
-        alt="Charizard"
-        className="absolute top-4 -right-24 md:-right-32 w-72 h-72 md:w-60 md:h-60 scale-x-[-1] z-0"
-        initial={{ x: "100%", opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ type: "spring", stiffness: 40, delay: 0.4 }}
-      />
-      <motion.img
-        src={eevee}
-        alt="Eevee"
-        className="absolute top-1/3 -right-16 md:-right-20 w-48 h-48 md:w-52 md:h-52 scale-x-[-1] z-0"
-        initial={{ x: "100%", opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ type: "spring", stiffness: 40, delay: 0.6 }}
-      />
-      <motion.img
-        src={mewto}
-        alt="Mewto"
-        className="absolute bottom-8 -right-20 md:-right-24 w-64 h-64 md:w-48 md:h-48 scale-x-[-1] z-0"
-        initial={{ x: "100%", opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ type: "spring", stiffness: 40, delay: 0.8 }}
-      />
+        {/* --- Imágenes de la Derecha --- */}
+        <motion.img
+          src={charizard}
+          alt="Charizard"
+          className="absolute top-4 -right-24 md:-right-32 w-72 h-72 md:w-60 md:h-60 scale-x-[-1] z-0"
+          initial={{ x: "100%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 40, delay: 0.4 }}
+        />
+        <motion.img
+          src={eevee}
+          alt="Eevee"
+          className="absolute top-1/3 -right-16 md:-right-20 w-48 h-48 md:w-52 md:h-52 scale-x-[-1] z-0"
+          initial={{ x: "100%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 40, delay: 0.6 }}
+        />
+        <motion.img
+          src={mewto}
+          alt="Mewto"
+          className="absolute bottom-8 -right-20 md:-right-24 w-64 h-64 md:w-48 md:h-48 scale-x-[-1] z-0"
+          initial={{ x: "100%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 40, delay: 0.8 }}
+        />
 
-      {/* 3. Contenido Central (con z-index para que esté por encima) */}
-      <div className="relative z-10 flex flex-col items-center justify-center text-center">
-        <motion.h1
-          initial={{ y: -50, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 1 }}
-          className="text-5xl md:text-6xl font-pokemon text-pokemon-yellow mb-4"
+        {/* 3. Contenido Central (con z-index para que esté por encima) */}
+        <div className="relative z-10 flex flex-col items-center justify-center text-center">
+          <motion.h1
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1 }}
+            className="text-5xl md:text-6xl mb-4 tracking-widest font-pokemon text-pokemon-yellow"
           style={{
             textShadow: `
-              -2px -2px 0 #2A75BB, 2px -2px 0 #2A75BB,
-              -2px 2px 0 #2A75BB, 2px 2px 0 #2A75BB,
+              -4px 4px 0 #2A75BB, 2px -2px 0 #2A75BB,
+              -4px 4px 0 #2A75BB, 2px 2px 0 #2A75BB,
               3px 3px 6px rgba(0,0,0,0.7)
             `,
-          }}
-        >
-          Bienvenido al Mundo Pokémon
-        </motion.h1>
-        <motion.p
-          initial={{ y: -30, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 1.2 }}
-          className="text-xl text-pokemon-blue mb-8"
-          style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.7)" }}
-        >
-          Tu aventura en la Pokédex comienza aquí.
-        </motion.p>
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ type: "spring", stiffness: 260, damping: 20, delay: 1.4 }}
-        >
-          <Link
-            to="/pokedex"
-            className="bg-pokemon-blue text-white font-bold rounded-full shadow-poke px-8 py-4 hover:shadow-glow-electric hover:scale-105 transition-all duration-300 transform"
+            }}
           >
-            ¡Explora la Pokédex!
-          </Link>
+            Bienvenido al Mundo Pokémon
+          </motion.h1>
+          <motion.p
+            initial={{ y: -30, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.2 }}
+            className="text-xl text-pokemon-blue mb-8"
+            style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.7)" }}
+          >
+            Tu aventura en la Pokédex comienza aquí.
+          </motion.p>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+              delay: 1.4,
+            }}
+          >
+            <Link
+              to="/pokedex"
+              className="bg-pokemon-blue text-white font-bold rounded-full shadow-poke px-8 py-4 hover:shadow-glow-electric hover:scale-105 transition-all duration-300 transform"
+            >
+              ¡Explora la Pokédex!
+            </Link>
+          </motion.div>
+        </div>
+      </div>
+      {/* Sección del Pokémon del Día */}
+      <div className="w-full bg-pokemon-blue py-16">
+        <motion.div
+          className="container mx-auto text-center px-4"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          {loading ? (
+            <Loader />
+          ) : (
+            randomPokemon && <FeaturedPokemon pokemon={randomPokemon} />
+          )}
         </motion.div>
       </div>
-    </div>
+    </>
   );
 };
 
